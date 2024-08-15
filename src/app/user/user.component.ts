9a2700200ca99cd57286c25fa26946225bd0fccd
Emmanuel Muyengwa <muyengwatakunda@gmail.com>
2024-08-15 16:22:58 +0200

    new components

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required:true}) id!: string;
+ @Input({required: true}) avatar!: string;
+ @Input({required:true}) name!: string;
+ @Output() select = new EventEmitter<string>();
+
+
+
+
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+}
+
+  onSelectUser() {
+     this.select.emit(this.id);
+  }
+}
