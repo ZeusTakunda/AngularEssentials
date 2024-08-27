420c7280c457acecd9c78eab88da256579311c04
Emmanuel Muyengwa <muyengwatakunda@gmail.com>
2024-08-27 10:28:36 +0200

    New task changes

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  
+  @Output() cancel =  new EventEmitter<void>()
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
+
+   onCancel() {
+     this.cancel.emit();
+  }
 }
